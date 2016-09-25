f5790bc24d0d2a0c3bdac02886a3faaeacdd3457
Tomasz Nowak <tomasz.nowak.tn1@contractors.roche.com>
2016-09-25 11:45:51 +0200

    Renamed the application

--- src/main/java/com/keyworld/BookcaseApplication.java
+++ src/main/java/com/keyworld/BookcaseApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookcaseSbApplication {
+public class BookcaseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookcaseSbApplication.class, args);
+		SpringApplication.run(BookcaseApplication.class, args);
 	}
 }
--- src/test/java/com/keyworld/BookcaseApplicationTests.java
+++ src/test/java/com/keyworld/BookcaseApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BookcaseSbApplicationTests {
+public class BookcaseApplicationTests {
 
 	@Test
 	public void contextLoads() {
