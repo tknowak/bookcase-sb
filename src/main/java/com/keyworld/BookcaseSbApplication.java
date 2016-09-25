dd8ecb916df6fb0df10f59588bf2f3d4db87e6ad
Tomasz Nowak <tomasz.nowak.tn1@contractors.roche.com>
2016-09-25 11:14:53 +0200

    Initialized Spring Boot

---
+++ src/main/java/com/keyworld/BookcaseSbApplication.java
@@ -0,0 +1,12 @@
+package com.keyworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcaseSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcaseSbApplication.class, args);
+	}
+}
