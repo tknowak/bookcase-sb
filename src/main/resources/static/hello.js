85eb478756a35b82f970c4bc0b314668cee9f575
MarcinGuth <marcinguth@yahoo.de>
2016-11-19 11:51:27 +0000

    Calling books REST endpoint from Angular

---
+++ src/main/resources/static/hello.js
@@ -0,0 +1,7 @@
+var app = angular.module("myApp", [])
+.controller('Hello', function($scope, $http) {
+     $http.get('http://localhost:8080/bookcase/books').
+             then(function(response) {
+                 $scope.books = response.data;
+             })
+     })
